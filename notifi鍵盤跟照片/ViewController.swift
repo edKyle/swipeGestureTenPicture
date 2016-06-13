64e4ac56f61f5d555560eb1e017029b4021f2fc0
edKyle <soonstart@hotmail.com>
2016-06-13 16:33:27 +0800

    Initial Commit

---
+++ notifi鍵盤跟照片/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  notifi鍵盤跟照片
+//
+//  Created by Kyle on 2016/6/13.
+//  Copyright © 2016年 Alphacamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
